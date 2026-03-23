e080d23b1e47405d32cedf35d31a6b7e0dfe15d4
Tonex5000 <108904318+Tonex5000@users.noreply.github.com>
2026-03-23 06:45:20 +0000

    changed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
   isMainnet: true,
   devNetRpcUrl: "https://api.devnet.solana.com/",
-  mainNetRpcUrl: "https://solana-mainnet.g.alchemy.com/v2/EwWTbSOeruRkG_WlIGicc",
+  mainNetRpcUrl: "https://solana-mainnet.core.chainstack.com/ad3af3f2a55be0eb86ed506cd86a0882",
 };
